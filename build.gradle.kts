17b4b685abe73e2673eb7bed035df0b02489959c
Len <40720638+destro174@users.noreply.github.com>
2024-01-06 10:24:14 +0100

    Update for minecraft 1.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,38 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
 import java.io.ByteArrayOutputStream
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
-    id("xyz.jpenilla.run-paper") version "1.0.6"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.2.2"
 }
 
 group = "com.semivanilla.chatitem"
-version = "1.0-SNAPSHOT"
+version = "2.0-SNAPSHOT"
 description = "Show an item preview in chat."
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT") // Paper
-    shadow("net.kyori:adventure-text-minimessage:4.10.0-SNAPSHOT") { // Minimessage
-        exclude("net.kyori", "adventure-api")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT") // Paper
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 tasks {
 
     runServer {
-        minecraftVersion("1.18.1")
+        minecraftVersion("1.20.4")
     }
 
-    shadowJar {
-        dependsOn(getByName("relocateJars") as ConfigureShadowRelocation)
-        archiveFileName.set("${project.name}-${project.version}.jar")
-        minimize()
-        configurations = listOf(project.configurations.shadow.get())
-    }
-
-    build {
-        dependsOn(shadowJar)
-    }
-
-    create<ConfigureShadowRelocation>("relocateJars") {
-        target = shadowJar.get()
-        prefix = "${project.name}.lib"
-    }
 }
 
 bukkit {
     name = rootProject.name
     main = "$group.${rootProject.name}"
     version = "${rootProject.version}-${gitCommit()}"
-    apiVersion = "1.18"
+    apiVersion = "1.20"
     website = "https://github.com/SemiVanilla-MC/${rootProject.name}"
     authors = listOf("destro174")
 }
